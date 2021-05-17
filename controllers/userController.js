73762012dc54ff89d8adad350d38235a269e2ffc
Bradley Pirie <bradleyp@bbd.co.za>
2021-05-17 12:16:08 +0200

    Console error fix

--- controllers/userController.js
+++ controllers/userController.js
@@ -4,18 +4,19 @@ const dao = UsersDAO.getInstance();
 
 exports.login = (req, res) => {
   const { name, email, token } = req.body;
-  google.verify(token)
+  google
+    .verify(token)
     .then(async () => {
       let user = await dao.findUserByEmail(email);
-      if (user === undefined) user =  await dao.insertUser(name, email);
+      if (user === undefined) user = await dao.insertUser(name, email);
       res.cookie("auth-token", token);
       res.status(200).send("User authenticated!");
-      res.send("index");
-
+      // TODO: Create and use Session, will go up in next PR
+      //req.session.authID = await bcrypt.hash(user.id + "", 10);
+      console.log("Session Created: " + req.session.authID);
     })
     .catch(() => {
-      res.status(401).send("User not authenticated!");
-      res.send("index");
       console.error;
     });
 };
+
