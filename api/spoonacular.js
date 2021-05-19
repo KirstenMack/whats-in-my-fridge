632801d58623662363d4db5d473d51fcc0db3791
Kirsten McCormack <kirstenm@bbd.co.za>
2021-05-19 12:04:21 +0200

    Implemented summary search

--- api/spoonacular.js
+++ api/spoonacular.js
@@ -5,13 +5,23 @@ apiKey = process.env.API_KEY;
 const searchRequest = (searchText) => {
     const encodedText = encodeURIComponent(searchText);
     try {
-        return axios.get(`https://api.spoonacular.com/recipes/findByIngredients?ingredients=${encodedText}&apiKey=${apiKey}`);
+        const number = 1;
+        return axios.get(`https://api.spoonacular.com/recipes/findByIngredients?ingredients=${encodedText}&number=${number}&apiKey=${apiKey}`)
     }
     catch(err){
         return  err;
     }
 }
 
+const summaryRequest = (id) => {
+    try {
+        return axios.get(`https://api.spoonacular.com/recipes/?id=${id}/summary&apiKey=${apiKey}`);
+    }catch (err){
+        return err;
+    }
+}
+
 module.exports = {
-    searchRequest
+    searchRequest,
+    summaryRequest
 }
--- controllers/searchController.js
+++ controllers/searchController.js
@@ -1,27 +1,48 @@
 'use strict'
 
 const spoonacular = require('../api/spoonacular')
+const axios = require("axios");
 
 exports.search = function(req, res) {
     try {
         const searchTerm = req.body.Ingredients;
         spoonacular.searchRequest(searchTerm)
-            .then((response) => {
-            if (response.data.length > 0) {
-                const result = response.data[0].title;
-                const image = response.data[0].image;
-                console.log(result);
-                res.render('index', {
-                    response: result,
-                    image: image
-                });
-            }
-            else {
-                res.render('index', {
-                    response: 'No matching result'
-                });
-            }
-        });
+            .then(response => {
+                    var recipes = []
+                    for( var i = 0; i < response.data.length; i++){
+                        var id = response.data[i].id;
+                        spoonacular.summaryRequest(id)
+                            .then( description => {
+                                console.log(description.data[0])
+                                // var recipe = {
+                                //     "id": id,
+                                //     "title": response.data[i].title,
+                                //     "image": response.data[i].image,
+                                //     "description": description
+                                // };
+                                // recipes.push(recipe);
+                            })
+                    }
+                    // return recipes
+                })
+            // .then((response) => {
+            // if (response.length > 0) {
+            //     const title = response[0].title;
+            //     const image = response[0].image;
+            //     const description = response[0].description;
+            //     console.log(description);
+            //     res.render('index', {
+            //         name: title,
+            //         image: image,
+            //         // description: summary
+            //     });
+            // }
+            // else {
+            //     res.render('index', {
+            //         name: 'No matching result'
+            //     });
+            // }
+        // });
     } catch (err) {
         console.log(err)
     }
