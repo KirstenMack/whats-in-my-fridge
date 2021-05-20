9bdef3fabf0a4d22a042a68d5fd0d0fa84789546
Bradley Pirie <bradleyp@bbd.co.za>
2021-05-20 17:35:53 +0200

    machine was problem

--- controllers/ingredientController.js
+++ controllers/ingredientController.js
@@ -8,52 +8,44 @@
 //         contents:contents
 //     });
 
+const connection = require("../db/connection");
+exports.view = async (req, res) => {
+  //  res.render('index');
 
-  const connection = require("../db/connection");
-exports.view =  async (req, res) => {
-//  res.render('index');
+  console.log("hey2");
 
+  //connect to db
+  //  const user = req.session.authID;
 
-console.log("hey2");
- 
-
-//connect to db
-//  const user = req.session.authID;
-
-connection.query('SELECT * FROM user', (err,rows) =>{
-  // connection.release();
-  console.log("heye");
-  if(!err) {
-      console.log("hey")
-      return rows;
-  } else {
+  connection.query("SELECT * FROM users", (err, rows) => {
+    // connection.release();
+    console.log("heye");
+    if (!err) {
+      console.log("change");
+      // console.log(rows);
+      // res.render('index', {rows});
+    } else {
       console.log(err);
-  }
+    }
+    console.log("the data from the user table: \n", rows);
+  });
 
-  console.log("the data from the user table: \n", rows);
-});
- 
-//  pool.getConnection((err, connection) =>{
-//     if(err) throw err;
-//     console.log("connected. " + connection.threadId);
+  //  pool.getConnection((err, connection) =>{
+  //     if(err) throw err;
+  //     console.log("connected. " + connection.threadId);
 
-//     console.log("hey")
-//     connection.query('SELECT * FROM user', (err,rows) =>{
-//         // connection.release();
-//         console.log("heye");
-//         if(!err) {
-//             console.log("hey")
-//             res.send({rows});
-//         } else {
-//             console.log(err);
-//         }
+  //     console.log("hey")
+  //     connection.query('SELECT * FROM user', (err,rows) =>{
+  //         // connection.release();
+  //         console.log("heye");
+  //         if(!err) {
+  //             console.log("hey")
+  //             res.send({rows});
+  //         } else {
+  //             console.log(err);
+  //         }
 
-//         console.log("the data from the user table: \n", rows);
-//     });
-//   });
-
-
+  //         console.log("the data from the user table: \n", rows);
+  //     });
+  //   });
 };
-
- 
-  
