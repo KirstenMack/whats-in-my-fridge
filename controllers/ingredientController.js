1bdcd18ef8e269348a241df18f5fe2452f4d6337
sambom02 <Kerane.sambo@vcontractor.co.za>
2021-05-21 08:50:08 +0200

    MIRCALES

--- controllers/ingredientController.js
+++ controllers/ingredientController.js
@@ -4,23 +4,20 @@ const {
 } = require('mysql');
 const connection = require('../db/connection');
 
-
-//connect pool
-const pool = createPool({
-  host: "localhost",
-  user: "root",
-  password: "",
-  database: "test",
-  connectionLimit: 10
-});
+// connection.query
+// //connect pool
+// const pool = createPool({
+//   host: "eu-cdbr-west-01.cleardb.com",
+//   user: "bb5dfd5ac8ffb4",
+//   password: "",
+//   database: "test",
+//   connectionLimit: 10
+// });
 
 
 exports.view = (req, res) => {
-  // res.render('index');
-
-  pool.getConnection((err, connection) => {
-    if (err) throw err;
-    console.log('Connected as ID ' + connection.threadId);
+    // if (err) throw err;
+    // console.log('Connected as ID ' + connection.threadId);
 
     connection.query('SELECT * FROM ingredients ', (err, rows) => {
       if (!err) {
@@ -33,7 +30,6 @@ exports.view = (req, res) => {
       console.log(rows);
       console.log("rows here");
     });
-  });
 
 }
 
@@ -44,12 +40,6 @@ exports.insert = (req, res) => {
   console.log(req.body);
   console.log(ingredient);
   console.log(unit);
-  pool.getConnection((err, connection) => {
-    if (err) throw err;
-    console.log('Connected as ID ' + connection.threadId);
-
-    // let searchTerm = req.body.search;
-
     user_id = 2;
 
     connection.query('INSERT INTO ingredients SET user_id =? ,ingredient_name = ?, quantity =?', [user_id, ingredient, unit], (err, rows) => {
@@ -65,13 +55,11 @@ exports.insert = (req, res) => {
       }
       console.log("rows inserted here");
     });
-  });
+  
 
 }
 
 exports.delete = (req, res) => {
-  pool.getConnection((err, connection) => {
-    if (err) throw err;
     console.log('Connected as ID ' + connection.threadId);
     // let searchTerm = req.body.search;
     // user_id = 2
@@ -85,6 +73,4 @@ exports.delete = (req, res) => {
       }
       console.log("rows deleted here");
     });
-  });
-
 }
